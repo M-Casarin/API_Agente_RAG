e7acef92c6f07fc07ae53f07bb61a3b47ebabdb7
M-Casarin <mauricio.casarin@ciencias.unam.mx>
2025-07-06 21:35:11 -0600

    Add: Implement QueryRequest and QueryResponse models in RequestRAG.py

---
+++ src/models/RequestRAG.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+
+class QueryRequest(BaseModel): 
+    question: str 
+
+
+class  QueryResponse(BaseModel): 
+    question: str 
+    answer: str 
+    # context: list[str]
+
